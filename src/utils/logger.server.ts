fa00bdefe9015a694da6aa5fb2f5eefb2906adb5
Ju Reis <j.reis@kroton.com.br>
2025-02-12 15:14:11 -0300

    Corrige configuração de logs para ambiente serverless

--- src/utils/logger.server.ts
+++ src/utils/logger.server.ts
@@ -3,17 +3,14 @@ import pino from "pino";
 // Verifica se está em ambiente de desenvolvimento
 const isDev = process.env.NODE_ENV !== "production";
 
-const logger = pino(
-	{
-		level: "info",
-		transport: isDev
-			? {
-					target: "pino-pretty",
-					options: { colorize: true },
-			  }
-			: undefined,
-	},
-	isDev ? undefined : pino.destination("./logs/app.log")
-); // Só salva logs no arquivo em produção
+const logger = pino({
+	level: "info",
+	transport: isDev
+		? {
+				target: "pino-pretty",
+				options: { colorize: true }, // Logs coloridos no console no modo dev
+		  }
+		: undefined, // Logs padrão no console em produção
+});
 
 export default logger;
